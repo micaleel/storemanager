df1bf27ebcc7bc822415f3d28d7d3dbce3a80736
micaleel <micaleel@localhost>
2013-09-02 15:57:15 +0100

    Movements v1

--- StoreManager/Models/Movement.cs
+++ StoreManager/Models/Movement.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StoreManager.Models {
 
     public class Movement {
         public int Id { get; set; }
+
+        [Display(Name = "Date Created")]
         public DateTime DateCreated { get; set; }
+
         public int StockId { get; set; }
         public int LocationId { get; set; }
         public string Notes { get; set; }
